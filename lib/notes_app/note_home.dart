8e2e6960326b4c00dbf8f7d5b63948a9da9653e0
AhmedKaion <AhmedKaion>
2024-01-11 18:00:13 +0200

    home

--- lib/notes_app/note_home.dart
+++ lib/notes_app/note_home.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:practice/notes_app/notes_body.dart';
 
 class NoteHome extends StatelessWidget {
   const NoteHome({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(onPressed: (){},child: Icon(Icons.add),
+      ),
+   body:NotesBody() ,
+    );
   }
 }
